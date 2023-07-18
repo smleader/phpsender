2960865b16614a30cc4aeed4291af71f0d06f716
adezayd <120370323+adezayd@users.noreply.github.com>
2023-07-18 08:58:54 +0700

    Initial commit

---
+++ mail.php
@@ -0,0 +1,74 @@
+<?php
+	require 'PHPMailer/PHPMailerAutoload.php';
+	// enter your credentials
+	$email = "useremail";
+	$password = "passwordemail";
+	// initialize the object
+	$mail = new PHPMailer;
+	$mail->isSMTP();
+	$mail->Host = 'mail.exchange.lintasarta.net';
+	$mail->Port = 587;
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	$mail->SMTPSecure = 'tls';
+	//$mail->SMTPSecure = 'ssl';
+	$mail->SMTPAuth = true;
+	//$mail->AuthType   = 'LOGIN';
+	//$mail->CharSet = "UTF-8";
+	$mail->Username = $email;
+	$mail->Password = $password;
+	// enter your name to be shown
+	$mail->FromName = "Ade";
+	$mail->From = $email;
+	
+
+	
+	
+
+	// csv input
+	if(!isset($argv[1])){
+		echo "Input Data list Not Specified";
+		die();
+	}
+	$toReadFile = trim($argv[1]);
+
+	// mail template\content
+	if(!isset($argv[2])){
+		echo "Input Mail template Not Specified";
+		die();
+	}
+	$mailTemplate = trim($argv[2]);
+	
+	// mail subject
+	if(!isset($argv[3])){
+		echo "Subject for mail Not Specified";
+		die();
+	}
+	$subject = trim($argv[3]);
+
+    $fh = fopen($toReadFile,'r');
+    while ($line = fgets($fh)) {
+    	$line=trim($line);
+    	$line = explode(',', $line);
+    	// get the data from the csv
+	$toEmailId = $line[1];
+	$toName = $line[0];
+		
+	// sample template.html to be send
+	$message = file_get_contents($mailTemplate);
+	$message = trim($message);
+	// add custom details to the mail
+	$message = str_replace('<!--name-->', $toName, $message);
+
+	$mail->clearAllRecipients();
+	$mail->addAddress($toEmailId, $toName);
+	$mail->Subject = $subject;
+	$mail->msgHTML($message);
+	// test whther the mail is send succesfully or not
+	if (!$mail->send()) {
+		$error = $toEmailId."\nMailer Error: " . $mail->ErrorInfo."\n\n";
+		echo  $error;
+	} else {
+		echo "To ".$toName."\nEmail: ".$toEmailId."\nRequest sent Successfully\n\n";
+	}
+   }
+?>
